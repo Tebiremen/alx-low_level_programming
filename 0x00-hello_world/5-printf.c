55fbd66d1715f8ced8ace928d0115d0d638ae0dd
tebiremen <okojie.aitebiremen@gmail.com>
2022-06-02 16:50:59 -0700

    My commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * print with proper grammar, but the outcome is a place of art,
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a place of art,\n");
+	return (0);
+}
