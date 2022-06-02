3d35baa466925257b7280bffe399954217750661
tebiremen <okojie.aitebiremen@gmail.com>
2022-06-02 16:28:21 -0700

    My commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry point
+ * print programming is like building a multilingual puzzle
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
+
+
+
+
+
+
+
+
+
