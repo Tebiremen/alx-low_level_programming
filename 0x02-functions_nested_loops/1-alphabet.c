b05177173eef839c0bd22d908ea6dd04f3478484
tebiremen <okojie.aitebiremen@gmail.com>
2022-06-08 14:26:11 -0700

    My commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	white (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar('\n');
+}
