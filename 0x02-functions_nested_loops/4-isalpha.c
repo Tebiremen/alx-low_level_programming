dc1557e1fc7dd0a1cc4fcac157c73c8a877f41cb
tebiremen <okojie.aitebiremen@gmail.com>
2022-06-08 16:54:43 -0700

    My commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,4 +3,22 @@
 /**
  * _isalpha - Return 1 if c is a letter. lowercase or uppercase
  *
- * @c: The int t
+ * @c: The int to print
+ * Return: Always 0.
+ */
+
+int _isalpha(int c)
+
+{
+
+	if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+}
