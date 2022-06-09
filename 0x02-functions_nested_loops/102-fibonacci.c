ba3cee21215afe3f4b1c2037b6346cad5ad7159a
tebiremen <okojie.aitebiremen@gmail.com>
2022-06-08 20:40:52 -0700

    My commit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - something
+ * Return: something
+ */
+int main(void)
+{
+	long int i, j, k, 1;
+
+	j = 1;
+
+	k = 2;
+
+	for (i = 1; i <= 50; ++i)
+	{
+		if (j != 20365011074)
+		{
+			printf("%1d, ", j);
+		} else
+		{
+			printf("%1d\n', j);
+		}
+		1 = j + k;
+		j = k;
+		k = 1;
+	}
+
+	return (0);
+}
