a266d0bdf56536f31da49efa77151ee55c2e4156
tebiremen <okojie.aitebiremen@gmail.com>
2022-07-18 18:10:23 -0700

    Quite stressful

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,17 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a linked list
- * @h: linked list of type listint_t to print
- *
- * Return: number of nodes
+ * print_listint - print the elements of linked list and return # of elements
+ * @h: pointer to the first element
+ * Return: number of elements
  */
+
 size_t print_listint(const listint_t *h)
 {
-    size_t num = 0;
+	int i;
 
-    while (h)
-    {
-        printf("%d\n", h->n);
-        num++;
-        h = h->next;
-    }
+	for (i = 0; h; h = (*h).next, i++)
+		printf("%d\n", (*h).n);
 
-    return (num);
+	return (i);
 }
--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,20 +1,17 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
- *
- * Return: number of nodes
+ * listint_len - return # of elements
+ * @h: pointer to the first element
+ * Return: number of elements
  */
+
 size_t listint_len(const listint_t *h)
 {
-    size_t num = 0;
+	int i;
 
-    while (h)
-    {
-        num++;
-        h = h->next;
-    }
+	for (i = 0; h; h = (*h).next, i++)
+		;
 
-    return (num);
+	return (i);
 }
